abd79649ac8b4d7447ace0245af228172386b6bf
segnitsega <segnitsega6@gmail.com>
2025-03-16 09:17:28 -0700

    add mongodb configuration

---
+++ config/db.js
@@ -0,0 +1,19 @@
+require('dotenv').config()
+const mongoose = require('mongoose')
+
+
+const connectDb = async() => {
+    try{
+        await mongoose.connect(process.env.MONGO_URI)
+        console.log("Mongodb connected") 
+    }
+    catch(err){
+        console.log("Unable to connect to mongodb", err)
+        process.exit(1)
+    }
+
+}
+
+ 
+
+
