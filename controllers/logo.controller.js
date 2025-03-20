35e002a434ed44bd8cedd941bbdaf5c3833bb0e3
segnitsega <segnitsega6@gmail.com>
2025-03-20 01:20:41 -0700

    add logo upload feature for jobs

--- controllers/logo.controller.js
+++ controllers/logo.controller.js
@@ -1,3 +1,5 @@
+const logo = require('../models/logo.model')
+
 const uploadFile = (req, res, next) => {
     try {
       if (!req.file) {
@@ -13,4 +15,21 @@ const uploadFile = (req, res, next) => {
     }
   };
 
-module.exports = uploadFile
+const logoInfoToMongo = async (req, res) => {
+    try {
+        const newLogo = new logo({
+            filename: req.file.filename,
+            filepath: req.file.path,
+            mimetype: req.file.mimetype,
+            size: req.file.size
+        });
+
+        await newLogo.save(); 
+
+        res.json({ message: "Logo uploaded successfully", newLogo });
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    } 
+}
+
+module.exports = { uploadFile, logoInfoToMongo}
